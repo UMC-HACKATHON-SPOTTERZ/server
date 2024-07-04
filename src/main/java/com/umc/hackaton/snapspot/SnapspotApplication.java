baa51033bf4052651e0cb203653080ff5f28fe0f
wonjun0120 <wonjundero@gmail.com>
2024-07-04 15:15:24 +0900

    init commit

---
+++ src/main/java/com/umc/hackaton/snapspot/SnapspotApplication.java
@@ -0,0 +1,13 @@
+package com.umc.hackaton.snapspot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnapspotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnapspotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/umc/hackaton/snapspot/SnapspotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umc.hackaton.snapspot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnapspotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
