e94fa998cc82ada1830acd3992eeb0813c141950
wonjun0120 <wonjundero@gmail.com>
2024-07-05 04:26:31 +0900

    feat: 기능 추가 완료
    #33

--- src/main/java/com/umc/hackaton/snapspot/spot/controller/SpotController.java
+++ src/main/java/com/umc/hackaton/snapspot/spot/controller/SpotController.java
@@ -1,18 +1,20 @@
 package com.umc.hackaton.snapspot.spot.controller;
 
 import com.umc.hackaton.snapspot.spot.dto.SpotRequestDto;
+import com.umc.hackaton.snapspot.spot.entity.Spot;
 import com.umc.hackaton.snapspot.spot.service.SpotService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
+@Slf4j
 @RequestMapping("/api/v1/spots")
 public class SpotController {
 
@@ -26,6 +28,47 @@ public class SpotController {
             return ResponseEntity.ok().body("스팟 업로드 성공.");
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("스팟 업로드에 실패하였습니다.");
+        }
+    }
+
+    @GetMapping("/{spotId}")
+    public ResponseEntity<?> showSpot(
+            @PathVariable("spotId") Long spotId
+    ) {
+        try {
+            Spot spot = spotService.getSpot(spotId);
+            return ResponseEntity.ok().body(spot);
+        } catch (Exception e){
+            log.info("스팟 조회에 실패하였습니다.", e);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("스팟 조회에 실패하였습니다.");
+        }
+    }
+
+    @DeleteMapping("/{spotId}")
+    public ResponseEntity<?> deleteSpot(
+            @PathVariable("spotId") Long spotId
+    ) {
+        try {
+            spotService.deleteSpot(spotId);
+            return ResponseEntity.ok().body("success");
+        } catch (Exception e){
+            log.info("스팟 삭제에 실패하였습니다.", e);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("스팟 삭제에 실패하였습니다.");
+        }
+    }
+
+
+    @PatchMapping("/{spotId}")
+    public ResponseEntity<?> patchSpot(
+            @PathVariable("spotId") Long spotId,
+            @RequestBody SpotRequestDto dto
+    ) {
+        try {
+            Spot spot = spotService.updateSpot(spotId, dto);
+            return ResponseEntity.ok().body(spot);
+        } catch (Exception e){
+            log.info("스팟 수정에 실패하였습니다.", e);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("스팟 수정에 실패하였습니다.");
         }
     }
 }
