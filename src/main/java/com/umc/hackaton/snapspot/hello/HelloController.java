96a440d7d3a76ca79f49de5120d6a1d7fbf1ebe0
MJJ <m3k0813@gmail.com>
2024-07-04 16:34:48 +0900

    feat: 도커 세팅

---
+++ src/main/java/com/umc/hackaton/snapspot/hello/HelloController.java
@@ -0,0 +1,13 @@
+package com.umc.hackaton.snapspot.hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello, World!";
+    }
+}
