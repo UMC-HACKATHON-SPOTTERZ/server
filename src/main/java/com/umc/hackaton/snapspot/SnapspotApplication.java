2187f9b9d3ff05cc3968360fadb89c53f22db830
do-dop <pdy0207@naver.com>
2024-07-04 23:53:42 +0900

    merge: dev

--- src/main/java/com/umc/hackaton/snapspot/SnapspotApplication.java
+++ src/main/java/com/umc/hackaton/snapspot/SnapspotApplication.java
@@ -2,10 +2,10 @@ package com.umc.hackaton.snapspot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaAuditing
+@EnableJpaRepositories
 public class SnapspotApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/umc/hackaton/snapspot/user/service/UserService.java
+++ src/main/java/com/umc/hackaton/snapspot/user/service/UserService.java
@@ -1,22 +1,16 @@
 package com.umc.hackaton.snapspot.user.service;
 
-import com.umc.hackaton.snapspot.user.dto.request.ImageUploadDto;
-import com.umc.hackaton.snapspot.user.dto.request.LoginDto;
-import com.umc.hackaton.snapspot.user.dto.request.UserRequestDto;
-import com.umc.hackaton.snapspot.user.dto.response.UserResponseDto;
+import com.umc.hackaton.snapspot.user.dto.UserRequestDto;
 import com.umc.hackaton.snapspot.user.entity.User;
 import com.umc.hackaton.snapspot.user.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
 @Service
 @RequiredArgsConstructor
-@Slf4j
 public class UserService {
 
     private final UserRepository userRepository;
@@ -24,35 +18,13 @@ public class UserService {
 
     @Transactional
     public void signUp(UserRequestDto dto) {
+
+//        Optional<User> existingUser = userRepository.findByUserId(dto.getUserId());
+//        if (existingUser.isPresent()) {
+//            User user = existingUser.get();
+//        } else {
         User user = dto.toEntity(encoder);
         userRepository.save(user);
-    }
-
-    @Transactional
-    public UserResponseDto login(LoginDto dto) {
-        Optional<User> loginuser = userRepository.findByUsername(dto.getUsername());
-        if (loginuser.isEmpty()) {
-            throw new IllegalArgumentException("존재하지 않는 사용자입니다.");
-        }
-
-        if (!encoder.matches(dto.getPassword(), loginuser.get().getPassword())) {
-            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
-        }
-
-        return UserResponseDto.builder()
-                .id(loginuser.get().getId())
-                .username(loginuser.get().getUsername())
-                .build();
-    }
-
-    @Transactional
-    public void imageUploads(ImageUploadDto dto) {
-        Optional<User> user = userRepository.findById(dto.getId());
-        if (user.isEmpty()) {
-            throw new IllegalArgumentException("존재하지 않는 사용자입니다.");
-        }
-        user.get().updateProfileImg(dto.getImage());
-
     }
 }
 
