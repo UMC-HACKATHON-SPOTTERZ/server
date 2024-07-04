998d58e43f348d2824021952d717e08cb3e5e27e
wonjun0120 <wonjundero@gmail.com>
2024-07-04 18:16:23 +0900

    feat: Swagger 예시 Controller 업데이트
    HelloController Swagger 예시를 위해 업데이트
    
    #7

--- src/main/java/com/umc/hackaton/snapspot/hello/HelloController.java
+++ src/main/java/com/umc/hackaton/snapspot/hello/HelloController.java
@@ -1,12 +1,24 @@
 package com.umc.hackaton.snapspot.hello;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "Example", description = "Example API")
 @RestController
 public class HelloController {
 
     @GetMapping("/hello")
+    @Operation(summary = "예시 API", description = "예시 API에 대한 설명, hello world에서 처리")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Success"),
+            @ApiResponse(responseCode = "400", description = "Not Found")
+    })
     public String hello(){
         return "Hello, World!";
     }
