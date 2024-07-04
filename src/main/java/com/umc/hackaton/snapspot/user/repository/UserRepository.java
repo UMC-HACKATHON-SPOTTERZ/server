a660cece6d83f071510a5b6aacdbdff065672c50
do-dop <pdy0207@naver.com>
2024-07-04 23:53:38 +0900

    merge: dev

--- src/main/java/com/umc/hackaton/snapspot/user/repository/UserRepository.java
+++ src/main/java/com/umc/hackaton/snapspot/user/repository/UserRepository.java
@@ -6,8 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByUsername(String username);
     Optional<User> findByUserId(String userId);
-    User findUserById(Long id);
 
+    User findUserById(Long id);
 }
